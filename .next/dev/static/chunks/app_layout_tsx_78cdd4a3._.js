fad0b341317cf20aae3e6410b5fab44d82bedfe8
thiruvengatesh <mthiru2210@gmail.com>
2025-12-18 07:37:28 +0530

    FIX REMOVE .NEXT

--- .next/dev/static/chunks/app_layout_tsx_78cdd4a3._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__51d697a8._.css",
-  "static/chunks/node_modules_848acba3._.js",
-  "static/chunks/_3f071d25._.js"
-],
-    source: "dynamic"
-});
--- .next/dev/static/chunks/app_members_page_tsx_720a591b._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_526c09ab._.js",
-  "static/chunks/node_modules_99edfa4d._.js"
-],
-    source: "dynamic"
-});
