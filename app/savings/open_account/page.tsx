3ac3063cd6503c021742d786f45ad01544b5bab1
thiruvengatesh <mthiru2210@gmail.com>
2026-02-06 17:22:41 +0530

    update view member 1

--- app/savings/open_account/page.tsx
+++ app/savings/open_account/page.tsx
@@ -8,9 +8,17 @@ export default function SavingsOpenAccountPage() {
             <div className="flex-1 space-y-2 p-2 border rounded-lg bg-background">
                 <div className="grid grid-cols-3 gap-4 border-b ">
                     <div className="space-y-2 px-5 ">
-                        <Label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">Member No</Label>
+                        <Label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">Member No for sb</Label>
                         <input
                               id="MemberNo"
+                              placeholder=""
+                              className="border"
+                            />
+                    </div>
+                    <div className="space-y-2 px-5 ">
+                        <Label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">Member name</Label>
+                        <input
+                              id="MemberName"
                               placeholder=""
                               className="border"
                             />
